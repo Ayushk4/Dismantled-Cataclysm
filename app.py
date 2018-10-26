d57c807bc6372660ee46f48ce9f9732164c02a2f
Ayush Kaushal <ayushk4@gmail.com>
2018-10-27 03:07:03 +0530

    Link the tables

--- app.py
+++ app.py
@@ -2,18 +2,9 @@ from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.utils import secure_filename
 import json
 import os
+from ir import spacy_data
 
-
-UPLOAD_FOLDER = 'files_inputted/'
-ALLOWED_EXTENSIONS = set(['mp3', 'wav', 'ogg'])
-#////////////// check for video file extention
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-def allowed_file(filename):
-    return '.' in filename and \
-            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-
 
 
 def get_data():
@@ -39,68 +30,89 @@ def get_data():
         data_vol[keys]["id"] = keys
         data_array_vol.append(data_vol[keys])
 
-
-
     return data_array_vol, data_array_vic
+
 
 @app.route('/', methods=['GET'])
 @app.route('/home', methods=['GET'])
 def home():
     data_array_vol, data_array_vic = get_data()
+    #print(len(data_array_vic))
     return render_template("home.html", data_array_vic = data_array_vic, data_array_vol = data_array_vol)
 
 @app.route('/home', methods = ['POST'])
 def homeSubmit():
+    
+    new_data = request.form['victim_form']
+    print("\n\n\nasfsdgds\n\n")
+    print(new_data)
 
-    if 'file' not in request.files:
-        flash('No file part')
-        return redirect(request.url)
-    file = request.files['file']
-    # if user does not select file, browser also
-    # submit an empty part without filename
-    if file.filename == '':
-        flash('No selected file')
-        return redirect(request.url)
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        data_array_vol, data_array_vic = get_data()
-        return render_template("home.html", data_array_vic = data_array_vic, data_array_vol = data_array_vol)
+    name, activity, donation, add = spacy_data(new_data)
 
+    new_id = {}
+    new_id["vic" + str(i)] = {}
+    new_id["vic" + str(i)]["data"] = {}
+    new_id["vic" + str(i)]["data"]["name"] = name
+    new_id["vic" + str(i)]["data"]["ailment"] = activity
+    new_id["vic" + str(i)]["data"]["location"] = add
+    new_id["vic" + str(i)]["status"] = "Unanswered"
+    new_id["vic" + str(i)]["alloted_to"] = "None"
+    
 
-    #    flash('Success')
-    #    print("success")
-    #print("\n\n\nasfsdgds\n\n")
-    #new_data = request.form['victim_form']
-    #print(new_data)
-    else:
-        data_array_vol, data_array_vic = get_data()
-        return render_template("home.html", data_array_vic = data_array_vic, data_array_vol = data_array_vol)
-
-
-
-@app.route('/homeaddvol', methods = ['POST'])
-def homeaddvol():
-    vol_code = request.form["volun_code"]
-    data_array_vol, data_array_vic = get_data()
-    print(request.form)
-
-    volid_found == -1
-
-    for i in data_array_vol:
-        if i["ids"] == vol_code:
-            i["alloted"].append()
-            new_vol = {}
-            with open ("data/volunteer.json","r") as read_f:
-                new_vol = json.load( read_f)
-
-            new_vol[i["ids"]]["data"]["append"] = i["allotted"]
-            
-            with open ("data/volunteer.json","w") as wri:
-                json.dump(new_vol,wri)
+    data_vic = {}
+    with open ("data/victim.json","r") as read_f:
+        data_vic = json.load(read_f)
+    data_vic["vic" + str(i)] = new_id["vic" + str(i)]
+    with open("data/victim.json","w") as w:
+        json.dump(data_vic,w)
 
     data_array_vol, data_array_vic = get_data()
     return render_template("home.html", data_array_vic = data_array_vic, data_array_vol = data_array_vol)
+
+
+@app.route('/homeassign', methods = ['POST'])
+def homeassign():
+    
+    data_array_vol, data_array_vic = get_data()
+
+    vic_id_being_assigned = ''
+    for i in data_array_vic:
+        try:
+            vol_code = request.form[i["id"]]
+
+        except:
+            continue
+
+        vic_id_being_assigned = i["id"]
+
+        new_vic = {}
+        with open ("data/victim.json","r") as read_f:
+            new_vic = json.load( read_f)
+
+        new_vic[vic_id_being_assigned]["status"] = "Assigned to " + vol_code
+        new_vic[vic_id_being_assigned]["alloted_to"] = vol_code
+
+        with open ("data/victim.json","w") as wri:
+            json.dump(new_vic,wri)
+
+        break
+
+    if vol_code == "" :
+        for i in data_array_vol:
+            if i["ids"] == vol_code:
+                i["alloted"].append()
+                new_vol = {}
+                with open ("data/volunteer.json","r") as read_f:
+                    new_vol = json.load( read_f)
+
+                new_vol[i["ids"]]["data"]["append"] = i["allotted"]
+                
+                with open ("data/volunteer.json","w") as wri:
+                    json.dump(new_vol,wri)
+
+    data_array_vol, data_array_vic = get_data()
+    return render_template("home.html", data_array_vic = data_array_vic, data_array_vol = data_array_vol)
+
 
 
 @app.errorhandler(404)
@@ -108,6 +120,64 @@ def pageNotFound(e):
     return ("Page Does Not Exist")
 
 if __name__ == "__main__":
+    i=4
     app.run(debug=True)
 
-#get_data()
+
+
+
+# string =
+# UPLOAD_FOLDER = 'files_inputted/'
+# ALLOWED_EXTENSIONS = set(['mp3', 'wav', 'ogg'])
+# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+# def allowed_file(filename):
+#     return '.' in filename and \
+#             filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+
+
+
+
+# @app.route('/home', methods = ['POST'])
+# def homeSubmit():
+
+#     if 'file' not in request.files:
+#         flash('No file part')
+#         return redirect(request.url)
+#     file = request.files['file']
+#     # if user does not select file, browser also
+#     # submit an empty part without filename
+#     if file.filename == '':
+#         flash('No selected file')
+#         return redirect(request.url)
+#     if file and allowed_file(file.filename):
+#         filename = secure_filename(file.filename)
+#         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+#         data_array_vol, data_array_vic = get_data()
+#         return render_template("home.html", data_array_vic = data_array_vic, data_array_vol = data_array_vol)
+
+
+#     #    flash('Success')
+#     #    print("success")
+#     #print("\n\n\nasfsdgds\n\n")
+#     #new_data = request.form['victim_form']
+#     #print(new_data)
+#     else:
+#         data_array_vol, data_array_vic = get_data()
+#         return render_template("home.html", data_array_vic = data_array_vic, data_array_vol = data_array_vol)
+
+
+
+
+
+# @app.errorhandler(404)
+# def pageNotFound(e):
+#     return ("Page Does Not Exist")
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+# #get_data()
--- data/make_data.py
+++ data/make_data.py
@@ -1,20 +1,23 @@
 import json
 
 data = {
-    "vol1" : {
-        "data" : {"name" : "volunteer1"},
-        "alloted" : []
+    "vic1" : {
+        "data" : {"name" : "Nani", "location" : "IIT Kgp near MMM hall canteen", "ailment" : "broken leg"},
+        "status" : "Unanswered",
+        "alloted_to" : "None"
     },
-    "vol2" : {
-        "data" : {"name" : "volunteer2"},
-        "alloted" : []
+    "vic2" : {
+        "data" : {"name" : "Chinmoy", "location" : "Kharapur, near Dominoes", "ailment" : "Fell"},
+        "status" : "Unanswered",
+        "alloted_to" : "None"
     },
-    "vol3" : {
-        "data" : {"name" : "volunteer3"},
-        "alloted" : []
+    "vic3" : {
+        "data" : {"name" : "Apoorve", "location" : "Kharagpur city, near railway station", "ailment" : "lack of food supply"},
+        "status" : "Unanswered",
+        "alloted_to" : "None"
     }
 }
 
-with open("volunteer.json", "w") as write_file:
+with open("victim.json", "w") as write_file:
     json.dump(data,write_file)
 #print('\n')
